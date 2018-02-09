ea1a2c918ef9a7b4942ea77942b510b5c6ece1af
marcolamothe <35273938+marcolamothe@users.noreply.github.com>
2018-02-09 17:50:24 +0100

    exercice 5
    exercice 5 added

---
+++ exercices/exercice05/js/script.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function () {
+
+	
+	var timer = setInterval(function () {
+	        window.alert("add me on facebook: Marc-Olivier Lamothe !");
+	    },4500);
+
+	var timer = setInterval(function () {
+	        window.alert("also follow me on Instagram: marcolamothe221");
+	    },6000);
+
+	var timer = setInterval(function () {
+	        window.alert("Don't forget to hit me up on twitter! @Slampeetam (my gamer name)");
+	    },8500);
+
+	var timer = setInterval(function () {
+	        window.alert("Did I tell you I'm a streamer? Go hit that like button on www.twitch.tv/slampeetam !");
+	    },12000);
+
+
+
+});
